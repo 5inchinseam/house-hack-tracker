7b7c8dba28192a28d1599005fb89be58e6957a9d
5inchinseam <bryantkwai0@gmail.com>
2025-04-16 23:05:38 -0400

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to House Hack Analyzer</h1>
+      <p>This is a placeholder Home page. The full dashboard will load here.</p>
+    </div>
+  );
+}
