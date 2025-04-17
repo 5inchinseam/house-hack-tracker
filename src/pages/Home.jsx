b4ec159b7e8aa6068728c6d263ff8dbe72ea7bbe
5inchinseam <bryantkwai0@gmail.com>
2025-04-16 23:14:33 -0400

    Delete src/pages directory

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Welcome to House Hack Analyzer</h1>
-      <p>This is a placeholder Home page. The full dashboard will load here.</p>
-    </div>
-  );
-}
